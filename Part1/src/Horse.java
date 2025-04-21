8f99a27f28baf650e1848d10107ea0ba90a03757
Junyi Lu <ec24936@qmul.ac.uk>
2025-04-21 11:38:09 +0100

    Added functionality to the Horse class to allow the horse to be raced.

--- Part1/src/Horse.java
+++ Part1/src/Horse.java
@@ -5,72 +5,69 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Horse
-{
-    //Fields of class Horse
-    
-    
-      
-    //Constructor of class Horse
-    /**
-     * Constructor for objects of class Horse
-     */
-    public Horse(char horseSymbol, String horseName, double horseConfidence)
-    {
-       
-    }
-    
-    
-    
-    //Other methods of class Horse
-    public void fall()
-    {
-        
-    }
-    
-    public double getConfidence()
-    {
-        
-    }
-    
-    public int getDistanceTravelled()
-    {
-        
-    }
-    
-    public String getName()
-    {
-        
-    }
-    
-    public char getSymbol()
-    {
-        
-    }
-    
-    public void goBackToStart()
-    {
-        
-    }
-    
-    public boolean hasFallen()
-    {
-        
+public class Horse {
+    // === Private Fields ===
+    private String name;               // Horse's name
+    private char symbol;              // Unicode character representing the horse
+    private int distanceTravelled;    // How far the horse has moved
+    private boolean hasFallen;        // Whether the horse has fallen
+    private double confidence;        // Confidence level (0.0 - 1.0)
+
+    // === Constructor ===
+    public Horse(char horseSymbol, String horseName, double horseConfidence) {
+        this.symbol = horseSymbol;
+        this.name = horseName;
+        setConfidence(horseConfidence);     // Use setter to ensure valid range
+        this.distanceTravelled = 0;
+        this.hasFallen = false;
     }
 
-    public void moveForward()
-    {
-        
+    // === Public Methods ===
+
+    // Makes the horse fall
+    public void fall() {
+        hasFallen = true;
     }
 
-    public void setConfidence(double newConfidence)
-    {
-        
+    // Resets the horse to the start of the race
+    public void goBackToStart() {
+        distanceTravelled = 0;
+        hasFallen = false;
     }
-    
-    public void setSymbol(char newSymbol)
-    {
-        
+
+    // Moves the horse forward by 1 unit
+    public void moveForward() {
+        distanceTravelled += 1;
     }
-    
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public int getDistanceTravelled() {
+        return distanceTravelled;
+    }
+
+    public boolean hasFallen() {
+        return hasFallen;
+    }
+
+    public double getConfidence() {
+        return confidence;
+    }
+
+    // Setters
+    public void setSymbol(char newSymbol) {
+        symbol = newSymbol;
+    }
+
+    public void setConfidence(double newConfidence) {
+        // Clamp confidence to range [0.0, 1.0]
+        confidence = Math.min(Math.max(newConfidence, 0.0), 1.0);
+    }
 }
